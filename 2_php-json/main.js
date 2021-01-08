42889b765d27f0da591d948b26207c4fe83ed6f0
alessandro <alessandro.massarella@gmail.com>
2021-01-08 16:56:50 +0100

    2.

---
+++ 2_php-json/main.js
@@ -0,0 +1,20 @@
+const myApp = new Vue ({
+  el: '#myApp',
+
+  data: {
+    records: [],
+    // url: "https://flynn.boolean.careers/exercises/api/array/music",
+    url: "http://localhost/server.php",
+  },
+
+  mounted: function() {
+
+      axios
+            .get (this.url)
+            .then ((response)=> {
+
+              this.records = response.data.response
+              console.log(response)
+            })
+  },
+});
